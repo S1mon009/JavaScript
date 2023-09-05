1fd56431fff82b78103d1d101d0fcbf5a89342b7
S1mon009 <skaszy25@gmail.com>
2023-09-05 10:46:56 +0200

    Add files via upload

--- 01-electronic-clock/javascript/script.js
+++ 01-electronic-clock/javascript/script.js
@@ -1,11 +1,13 @@
-$(window).on('load',function(){
-  setInterval(function(){
-    const date=new Date();
-    const hour=date.getHours()<10? `0${date.getHours()}`:date.getHours();
-    const minute=date.getMinutes()<10? `0${date.getMinutes()}`:date.getMinutes();
-    const seconds=date.getSeconds()<10? `0${date.getSeconds()}`:date.getSeconds();
-    $('.hours').text(hour);
-    $('.minutes').text(minute)
-    $('.seconds').text(seconds)
-  },1000)
-})
+$(window).on("load", function () {
+  setInterval(function () {
+    const date = new Date();
+    const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    const minute =
+      date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds =
+      date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    $(".hours").text(hour);
+    $(".minutes").text(minute);
+    $(".seconds").text(seconds);
+  }, 1000);
+});
